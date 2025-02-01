8a37e3f0e36ce92a0940c1278cc64a94e0fadfb3
ayushraina2028 <ayushraina@iisc.ac.in>
2025-02-02 01:09:15 +0530

    PRAM Notes Added + Parallel Prefix Sum v1 and v2 added

---
+++ CUDA/Algorithms/PrefixSUM/PrefixSums1.cu
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+// CUDA Kernel
+/* 
+Parallel Programming is easy as long as you don't care about performance, In this version I will spawn n threads to calculate each partial prefix sum
+I know, I am going to implement this shit code but still for the purpose of learning, I will do
+ */
+
+__global__ void NaivePrefixSum(int* A, int* C) {
+    int index = threadIdx.x;
+
+    int sum = 0;
+    for(int i = 0;i <= index; i++) {
+        sum += A[i];
+    }
+
+    C[index] = sum;
+}
+
+high_resolution_clock::time_point getTime() {
+    return high_resolution_clock::now();
+}
+
+int main() {
+    int N = 10;
+    size_t size = N*sizeof(int);
+    int* A = (int*) malloc(size);
+    int* PreSUM = (int*) malloc(size);
+
+    // Initialize both to same data
+    for(int i = 0;i < N; i++) {
+        A[i] = i;
+        PreSUM[i] = i;
+    }
+
+    // CUDA Allocation + Copy
+    int* dA; cudaMalloc((void**) &dA, size); cudaMemcpy(dA, A, size, cudaMemcpyHostToDevice);
+    int* dPreSUM; cudaMalloc((void**) &dPreSUM, size); cudaMemcpy(dPreSUM, PreSUM, size, cudaMemcpyHostToDevice); 
+
+    int threadsPerBlock = N;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    // Calling 
+    NaivePrefixSum<<<blocksPerGrid, threadsPerBlock>>> (dA,dPreSUM);
+
+    // Copy Data Back
+    cudaMemcpy(PreSUM, dPreSUM, size, cudaMemcpyDeviceToHost); cudaFree(dA); cudaFree(dPreSUM);
+
+    cout << "Original Array: ";
+    for(int i = 0;i < N; i++) cout << A[i] << " ";
+    cout << endl;
+
+    cout << "Prefix Sum: ";
+    for(int i = 0;i < N; i++) cout << PreSUM[i] << " ";
+    cout << endl;
+
+    free(A); free(PreSUM);
+
+    return 0;
+}
---
+++ CUDA/Algorithms/PrefixSUM/PrefixSums2.cu
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+__global__ void PrefixSum2(int* A, int* C, int N) {
+    int index = threadIdx.x;
+    for(int stride = 1; stride < N; stride *= 2) {
+
+        if(index + stride < N) {
+            C[index + stride] += C[index];
+            printf("Added A[%d] to C[%d] \n", index, index+stride);
+        }
+
+    }
+}
+
+high_resolution_clock::time_point getTime() {
+    return high_resolution_clock::now();
+}
+int main() {
+    int N = 15;
+    size_t size = N*sizeof(int);
+    int* A = (int*) malloc(size);
+    int* PreSUM = (int*) malloc(size);
+
+    // Initialize both to same data
+    for(int i = 0;i < N; i++) {
+        A[i] = i;
+        PreSUM[i] = i;
+    }
+
+    // CUDA Allocation + Copy
+    int* dA; cudaMalloc((void**) &dA, size); cudaMemcpy(dA, A, size, cudaMemcpyHostToDevice);
+    int* dPreSUM; cudaMalloc((void**) &dPreSUM, size); cudaMemcpy(dPreSUM, PreSUM, size, cudaMemcpyHostToDevice); 
+
+    int threadsPerBlock = N;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    // Calling 
+    PrefixSum2<<<blocksPerGrid, threadsPerBlock>>> (dA,dPreSUM,N);
+
+    // Copy Data Back
+    cudaMemcpy(PreSUM, dPreSUM, size, cudaMemcpyDeviceToHost); cudaFree(dA); cudaFree(dPreSUM);
+
+    cout << "Original Array: ";
+    for(int i = 0;i < N; i++) cout << A[i] << " ";
+    cout << endl;
+
+    cout << "Prefix Sum: ";
+    for(int i = 0;i < N; i++) cout << PreSUM[i] << " ";
+    cout << endl;
+
+    free(A); free(PreSUM);
+
+    return 0;
+}
